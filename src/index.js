a1ae13ce9ffc34f28da029b6d54e16530a927da5
YuMichay <julia.one.such@gmail.com>
2021-08-23 14:42:32 +0300

    feat: add a condition for negative numbers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 module.exports = function reverse (n) {
+    if (n < 0){
+        let nString = n.toString()
+        .split("");
+        let arr = nString.splice(1);
+        return Number(arr.reverse().join(''));
+    }
     let arr = n.toString()
     .split("");
     return Number(arr.reverse().join(''));
